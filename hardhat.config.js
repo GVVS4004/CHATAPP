ff84bc2f9f016b2e86cc83c98604051e3ea1993a
GVVS4004 <gvijay4004@gmail.com>
2023-08-06 21:37:14 +0530

    version v3

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  paths:{
-    artifacts:"./artifacts",
-  },
-  networks:{
-    fuji:{
-      url: process.env.QUICKNODE_URL,
-      accounts: ['0x'+process.env.PRIVATE_KEY],
-      chainId: 43113,
-    }
-  }
+  // paths:{
+  //   artifacts:"./artifacts",
+  // },
+  // networks:{
+  //   fuji:{
+  //     url: process.env.QUICKNODE_URL,
+  //     accounts: ['0x'+process.env.PRIVATE_KEY],
+  //     chainId: 43113,
+  //   }
+  // }
 };
