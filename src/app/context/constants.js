290917965b5fb94216cc32e3f37887370e826616
GVVS4004 <gvijay4004@gmail.com>
2023-08-06 22:41:26 +0530

    production 1

--- src/app/context/constants.js
+++ src/app/context/constants.js
@@ -1,7 +1,8 @@
 // 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
 require("dotenv").config();
-import chatAppJSON from '../../../artifacts/contracts/ChatApp.sol/ChatApp.json'
+// import chatAppJSON from '../../../artifacts/contracts/ChatApp.sol/ChatApp.json'
+import chatAppJSON from './ChatApp.json'
 console.log(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
 const ChatAppAddress=process.env.CONTRACT_ADDRESS==null?process.env.NEXT_PUBLIC_CONTRACT_ADDRESS:process.env.CONTRACT_ADDRESS;
 const ChatAppABI = chatAppJSON.abi;
